acdf100f9f67865cb72dd3e743ec249a56462b2a
Harkirat <harkiratsingh20026@gmail.com>
2026-04-01 11:20:28 +0530

    done

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -16,6 +16,12 @@ public class Array {
         }
         return minVal;
     }
+    boolean isSorted(int[] arr){
+        for (int i = 0;i<arr.length;i++){
+            if (arr[i]>arr[i+1]) return false;
+        }return true;
+    }
+
 
     public static void main(String[] args) {
         System.out.println();
