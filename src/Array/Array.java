1f4fb93f1c5cecf564de335ab617901c546a7e3d
Harkirat <harkiratsingh20026@gmail.com>
2026-04-01 11:31:44 +0530

    done

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.ArrayList;
+
 public class Array {
     int largestEl(int[] arr){
         int minVal = Integer.MIN_VALUE;
@@ -21,6 +23,21 @@ public class Array {
             if (arr[i]>arr[i+1]) return false;
         }return true;
     }
+    int removeDuplicates(int[] arr){
+        int i=0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i]!=arr[j]) arr[i]=arr[j];
+        }
+        return i+1;
+    } ArrayList<Integer> removeDuplicatesWithArrayList(int[] arr){
+        ArrayList<Integer> al = new ArrayList<>();
+        int i=0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i]!=arr[j]) al.add(arr[j]);
+        }
+        return al;
+    }
+
 
 
     public static void main(String[] args) {
