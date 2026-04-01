40cabc241057b6e05ead85b8c5ef4cc7e14c91a4
Harkirat <harkiratsingh20026@gmail.com>
2026-04-01 11:18:07 +0530

    done

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,7 +1,23 @@
 package Array;
 
 public class Array {
-    public static void main(String[] args) {
+    int largestEl(int[] arr){
+        int minVal = Integer.MIN_VALUE;
+        for (int el:arr){
+            if (el>minVal) minVal = el;
+        }
+        return minVal;
+    }
+    int secondLargestEl(int[] arr){
+        int largest = largestEl(arr);
+        int minVal = Integer.MIN_VALUE;
+        for (int el:arr){
+            if (el!=largest || el>minVal) minVal = el;
+        }
+        return minVal;
+    }
 
+    public static void main(String[] args) {
+        System.out.println();
     }
 }
