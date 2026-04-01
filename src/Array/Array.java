5c34ab5dfa64e1288e2967c0aa59bdc3581fb795
Harkirat <harkiratsingh20026@gmail.com>
2026-04-01 11:39:43 +0530

    done

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -24,12 +24,16 @@ public class Array {
         }return true;
     }
     int removeDuplicates(int[] arr){
+        if(arr.length==0) return 0;
         int i=0;
         for (int j = 1; j < arr.length; j++) {
-            if (arr[i]!=arr[j]) arr[i]=arr[j];
+            if (arr[i]!=arr[j]) {
+                i++;
+                arr[i]=arr[j];}
         }
         return i+1;
-    } ArrayList<Integer> removeDuplicatesWithArrayList(int[] arr){
+    }
+    ArrayList<Integer> removeDuplicatesWithArrayList(int[] arr){
         ArrayList<Integer> al = new ArrayList<>();
         int i=0;
         for (int j = 1; j < arr.length; j++) {
