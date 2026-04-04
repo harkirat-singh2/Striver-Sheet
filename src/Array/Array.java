3fc35e6318eaa8c4278c2935f7fb64ec752f2bb8
Harkirat <harkiratsingh20026@gmail.com>
2026-04-04 22:39:11 +0530

    done

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -83,6 +83,7 @@ public class Array {
             }
         }
     }
+
     int linearSearch(int[] arr , int target){
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) return i;
