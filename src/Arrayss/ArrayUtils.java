80270a214b3655c615429f3c5977fb37335985ef
Harkirat <harkiratsingh20026@gmail.com>
2026-04-05 10:35:46 +0530

    done

--- src/Arrayss/ArrayUtils.java
+++ src/Arrayss/ArrayUtils.java
@@ -240,6 +240,22 @@ public class ArrayUtils {
         return maxLength;
     }
 
+    // 🔹 Number of  Subarray with Sum K (Optimal)
+    public int subarraySum(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+        map.put(0, 1); // important
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum-k)) {
+                count += map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         System.out.println("Array Utils Ready 🚀");
     }
