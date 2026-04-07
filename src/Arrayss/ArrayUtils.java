49124c1244c63a42de1652979e3ba409b0b72d57
Harkirat <harkiratsingh20026@gmail.com>
2026-04-07 08:36:55 +0530

    done

--- src/Arrayss/ArrayUtils.java
+++ src/Arrayss/ArrayUtils.java
@@ -271,6 +271,20 @@ public class ArrayUtils {
         }
         return maxLen;
     }
+    public int lengthOfLongestSubstringPractice(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLen=0;
+        int left=0;
+        for (int right = 0;  right <s.length() ; right++) {
+            char ch =s.charAt(right);
+            if (map.containsKey(ch)){
+                left=Math.max(left,map.get(ch)+1);
+            }
+            map.put(ch,right);
+            maxLen=Math.max(maxLen,right-left+1);
+        }
+        return maxLen;
+    }
 
 
     public static void main(String[] args) {
